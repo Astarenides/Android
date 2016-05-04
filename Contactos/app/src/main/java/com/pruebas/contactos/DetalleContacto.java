8952bc678d818c3a31b96f90b24fa812d1f75c0a
Fco. Javier Hidalgo <astarenides@hotmail.com>
2016-05-04 08:57:56 +0200

    Intents en contactos

--- Contactos/app/src/main/java/com/pruebas/contactos/DetalleContacto.java
+++ Contactos/app/src/main/java/com/pruebas/contactos/DetalleContacto.java
@@ -1,10 +1,21 @@
 package com.pruebas.contactos;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class DetalleContacto extends AppCompatActivity {
+
+    TextView txtNombre;
+    TextView txtTelefono;
+    TextView txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,17 +24,44 @@ public class DetalleContacto extends AppCompatActivity {
 
         Bundle parametros = getIntent().getExtras();
 
-        String nombre       = parametros.getString(getResources().getString(R.string.pnombre));       // Nombre
-        String telefono     = parametros.getString(getResources().getString(R.string.ptelefono));   // Telefono
-        String email        = parametros.getString(getResources().getString(R.string.pemail));         // Email
+        String nombre = parametros.getString(getResources().getString(R.string.pnombre));       // Nombre
+        String telefono = parametros.getString(getResources().getString(R.string.ptelefono));   // Telefono
+        String email = parametros.getString(getResources().getString(R.string.pemail));         // Email
 
-        TextView txtNombre      = (TextView) findViewById(R.id.txtNombre);
-        TextView txtTelefono    = (TextView) findViewById(R.id.txtTelefono);
-        TextView txtEmail       = (TextView) findViewById(R.id.txtEmail);
+        txtNombre = (TextView) findViewById(R.id.txtNombre);
+        txtTelefono = (TextView) findViewById(R.id.txtTelefono);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
 
         txtNombre.setText(nombre);
         txtTelefono.setText(telefono);
         txtEmail.setText(email);
 
+    }
+
+    public void llamar(View v) {
+        String telefono = txtTelefono.getText().toString();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telefono)));
+    }
+
+    public void enviarMail (View v) {
+        String correo = txtEmail.getText().toString();
+        Log.i("email", correo);
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{correo});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Asuntillo");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Texto del mensaje");
+        emailIntent.setType("message/rfc822");
+        startActivity(Intent.createChooser(emailIntent, "Email "));
     }
 }
